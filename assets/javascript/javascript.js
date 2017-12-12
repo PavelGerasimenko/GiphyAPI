a615595b5ae8ce3f5f2d2ae137cff91e65bf43c7
Pavel <pavel@Pavels-MacBook-Pro.local>
2017-12-11 21:04:15 -0800

    javascript

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+//Array for searched topics should be added
+//Function with AJAX call to GIPHY; Q parameter for API link set to search term, limit 10 res
+//Create div with respective still and animate image sources with "data-state", "data-still" and "data-animate" attributes
+//Submit button click event takes search term from form input, trims and pushes to topics array, displays button
+//Function iterates through topics array to display button with array values in "myButtons"
+//Click event on button with id of "country" executes displayCountry function
+//Click event on gifs with class executes pausePlayGifs function
+//Function accesses "data-state" attribute depending on status
+});
